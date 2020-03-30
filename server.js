8b7986854952f6235bfddb4c4657bb23d5502039
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-03-29 22:53:09 -0700

    add server.js file

--- server.js
+++ server.js
@@ -11,19 +11,32 @@ let corsOptions= {
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
-
 app.use(bodyParser.json());
 
 
 // parse requests of content-type - application/x-www-form-urlencoded
-
 app.use(bodyParser.urlencoded({ extended : true}));
 
-// simple route
+const db = require("./models");
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser : true,
+        useUnifiedTopology : true
+    })
+    .then(()=> {
+        console.log("Connected to the database!");
+    })
+    .catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    })
 
+// simple route
 app.get("/", (req, res) => {
     res.json({ message : "welcome to node/express rest api app"});
 });
+
+require("./routes/tutorial.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
